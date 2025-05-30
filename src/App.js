d1b1adbae6d9fdac6c4791a1a08fb72775c11756
kchett1 <145596310+kchett1@users.noreply.github.com>
2025-05-30 13:51:04 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DJ Photo - Photography Portfolio</h1>
+        <p>Coming Soon...</p>
+        <p>Professional photography showcasing beautiful destinations and moments.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
